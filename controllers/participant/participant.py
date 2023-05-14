5999b9e103789f4f971cb5b28c7b6febb7d7f1d3
Javilinos <javier.mdeza@alumnos.upm.es>
2023-05-14 15:19:59 +0200

    maybe torchvision succesfully installs cuda

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -215,7 +215,7 @@ class Wrestler(Robot):
         num_envs = 1
         # Episode start signals are used to reset the lstm states
         episode_starts = np.ones((num_envs,), dtype=bool)
-        model = RecurrentPPO.load("attacker_model/winner_model.zip", device=th.device("cpu"))
+        model = RecurrentPPO.load("attacker_model/winner_model.zip")
 
         while self.step(self.time_step) != -1 :  # mandatory function to make the simulation run
             if (self.fall_detector.check()):
