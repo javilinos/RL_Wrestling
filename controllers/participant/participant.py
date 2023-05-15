fef3a3dcf3d08e34f382b63aa36da1b0e2be65f9
Javilinos <javier.mdeza@alumnos.upm.es>
2023-05-15 11:59:20 +0200

    changed image, changed sb3 and sb3_contrib versions to my systems'

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -198,7 +198,7 @@ class Wrestler(Robot):
         # Episode start signals are used to reset the lstm states
         episode_starts = np.ones((num_envs,), dtype=bool)
         print ("Initializing RL model")
-        model = RecurrentPPO.load("winner_model.zip", features_extractor_class=CustomCNN)
+        model = RecurrentPPO.load("winner_model.zip")
 
         while self.step(self.time_step) != -1 :  # mandatory function to make the simulation run
             if (self.fall_detector.check()):
