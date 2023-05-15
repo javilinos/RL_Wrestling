9fe166f492a93b71b127404ce9b83c96d5bfceb8
Javilinos <javier.mdeza@alumnos.upm.es>
2023-05-15 02:38:50 +0200

    there might be a bug

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -113,24 +113,24 @@ class CustomCNN(BaseFeaturesExtractor):
         features_dim: int = 512,
         normalized_image: bool = False,
     ) -> None:
-        assert isinstance(observation_space, spaces.Box), (
-            "NatureCNN must be used with a gym.spaces.Box ",
-            f"observation space, not {observation_space}",
-        )
+        # assert isinstance(observation_space, spaces.Box), (
+        #     "NatureCNN must be used with a gym.spaces.Box ",
+        #     f"observation space, not {observation_space}",
+        # )
         super().__init__(observation_space, features_dim)
         # We assume CxHxW images (channels first)
         # Re-ordering will be done by pre-preprocessing or wrapper
-        assert is_image_space(observation_space, check_channels=False, normalized_image=normalized_image), (
-            "You should use NatureCNN "
-            f"only with images not with {observation_space}\n"
-            "(you are probably using `CnnPolicy` instead of `MlpPolicy` or `MultiInputPolicy`)\n"
-            "If you are using a custom environment,\n"
-            "please check it using our env checker:\n"
-            "https://stable-baselines3.readthedocs.io/en/master/common/env_checker.html.\n"
-            "If you are using `VecNormalize` or already normalized channel-first images "
-            "you should pass `normalize_images=False`: \n"
-            "https://stable-baselines3.readthedocs.io/en/master/guide/custom_env.html"
-        )
+        # assert is_image_space(observation_space, check_channels=False, normalized_image=normalized_image), (
+        #     "You should use NatureCNN "
+        #     f"only with images not with {observation_space}\n"
+        #     "(you are probably using `CnnPolicy` instead of `MlpPolicy` or `MultiInputPolicy`)\n"
+        #     "If you are using a custom environment,\n"
+        #     "please check it using our env checker:\n"
+        #     "https://stable-baselines3.readthedocs.io/en/master/common/env_checker.html.\n"
+        #     "If you are using `VecNormalize` or already normalized channel-first images "
+        #     "you should pass `normalize_images=False`: \n"
+        #     "https://stable-baselines3.readthedocs.io/en/master/guide/custom_env.html"
+        # )
         n_input_channels = observation_space.shape[0]
 
         self.cnn = nn.Sequential(
